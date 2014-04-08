84c49d61cb2bd0c2469bbcd058cbd573d2935009
Kameron Bain <kam@craaave.com>
2014-04-08 14:50:34 -0700

    Added Devise.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Temporal::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'http://kambain-temporal.herokuapp.com/' }
 end
